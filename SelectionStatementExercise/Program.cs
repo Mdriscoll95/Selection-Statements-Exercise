922db9e1a9c9e22fd179bf0f141be1deb63f53ae
Michael Driscoll <michael.driscoll951@gmail.com>
2023-05-15 12:49:09 -0700

    Selection

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("guess a number");
+            int favNumber = 13;
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == favNumber) 
+            {
+                Console.WriteLine($"Correct");
+            }
+            else
+            {
+                Console.WriteLine($"incorrect!");
+            }
         }
     }
 }
